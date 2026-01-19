262d39819b75f1f4989c94cb0d630178f41e51a3
Kavya R Menon <kavyarmenon98@gmail.com>
2026-01-19 11:59:24 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,7 +33,7 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <StrictMode >
@@ -41,5 +41,5 @@ createRoot(document.getElementById('root')).render(
         </StrictMode>
       </Provider>
     </QueryClientProvider>
-  </GoogleOAuthProvider>
+
 )
